db8ac495184125e0776ed1fcace5c916bcfa5551
Matheus Gomes <matheus.gom23@yahoo.com>
2023-02-02 20:08:16 -0300

    Novo Layout -Criação Class/Interface AuthenticateService

--- Services/IAlunoService.cs
+++ Services/IAlunoService.cs
@@ -9,6 +9,6 @@ namespace AlunosApi.Services
         Task<IEnumerable<Aluno>> GetAlunoByNome(string nome);
         Task CreateAluno(Aluno aluno);
         Task UpdateAluno(Aluno aluno);
-        Task DeleteAluno(Aluno aluno);   
+        Task DeleteAluno(Aluno aluno);
     }
 }
